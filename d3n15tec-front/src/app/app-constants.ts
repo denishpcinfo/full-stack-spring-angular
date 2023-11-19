d39f8a03f533baca663ed37dff33989729df8a41
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2023-11-19 19:54:01 -0300

    Update app-constants.ts

--- d3n15tec-front/src/app/app-constants.ts
+++ d3n15tec-front/src/app/app-constants.ts
@@ -1,7 +1,4 @@
 export class AppConstants {
 
 	public static get baseServidor(): string { return 'http://localhost:8080' }
-
-	//public static get baseServidor(): string { return 'http://d3n15-tec.us-east-2.elasticbeanstalk.com' }
-
 }
